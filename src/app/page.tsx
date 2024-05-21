aed6fc6243199bf4b80775b2d67b8f57089fd452
RJ Nay <rjnay1984@gmail.com>
2024-05-21 14:37:15 -0500

    shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import React from 'react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Welcome to frontend</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24 bg-library-splash bg-no-repeat bg-center">
+      <Card className='shadow-lg'>
+        <CardHeader>
+          <CardTitle>Welcome to Frontend</CardTitle>
+        </CardHeader>
+        <CardContent className="text-center">
+          <Button>Sign In</Button>
+        </CardContent>
+      </Card>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
 import type { Config } from 'tailwindcss';
+import TailwindCssAnimate from 'tailwindcss-animate';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'library-splash': "linear-gradient(to top, rgb(255, 255, 255, 0.5), rgb(255, 255, 255, 0.5)), url('/library-splash.jpg')",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [TailwindCssAnimate],
+} satisfies Config;
+
 export default config;
