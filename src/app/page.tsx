52b5d1c9d59b4d4fa85501b311bb17cdf775e690
RJ Nay <rjnay1984@gmail.com>
2024-05-21 15:16:41 -0500

    authjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,40 @@
+import { auth, signIn, signOut } from '@/auth';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import React from 'react';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24 bg-library-splash bg-no-repeat bg-center">
       <Card className="shadow-lg">
         <CardHeader>
-          <CardTitle>Welcome to Local Library</CardTitle>
+          <CardTitle>
+            Welcome to Local Library {session?.user && session.user.name}
+          </CardTitle>
         </CardHeader>
         <CardContent className="text-center">
-          <Button>Sign In</Button>
+          {session ? (
+            <form
+              action={async () => {
+                'use server';
+
+                await signOut();
+              }}
+            >
+              <Button>Sign Out</Button>
+            </form>
+          ) : (
+            <form
+              action={async () => {
+                'use server';
+
+                await signIn('authentik');
+              }}
+            >
+              <Button>Sign In</Button>
+            </form>
+          )}
         </CardContent>
       </Card>
     </main>
