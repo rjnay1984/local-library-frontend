37ec5917f8e565f175c2667a3c9f9c0277361828
RJ Nay <rjnay1984@gmail.com>
2024-05-28 15:50:24 -0500

    add token and groups to session (again)

--- src/auth.ts
+++ src/auth.ts
@@ -11,29 +11,39 @@ declare module 'next-auth' {
   }
 
   interface User {
-    groups: string[]
+    groups: string[];
   }
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Authentik({
-    profile(profile) {
-      return {
-        ...profile,
-        groups: profile.groups ?? ['user'],
-      }
-    }
-  })],
+  providers: [
+    Authentik({
+      profile(profile) {
+        return {
+          ...profile,
+          groups: profile.groups ?? ['user'],
+        };
+      },
+    }),
+  ],
   callbacks: {
     jwt({ token, user, account }) {
       let returnToken = token;
       if (user && account) {
-        returnToken = { ...token, groups: user.groups, accessToken: account.access_token };
+        returnToken = {
+          ...token,
+          groups: user.groups,
+          accessToken: account.access_token,
+        };
       }
       return returnToken;
     },
     session({ session, token }) {
-      return { ...session, accessToken: token.accessToken, user: { ...session.user, groups: token.groups as string[] }};
-    }
-  }
+      return {
+        ...session,
+        accessToken: token.accessToken,
+        user: { ...session.user, groups: token.groups as string[] },
+      };
+    },
+  },
 });
