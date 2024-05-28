97875f26001b9757f1d85a6a081c2228154d871a
RJ Nay <rjnay1984@gmail.com>
2024-05-28 15:50:06 -0500

    add token and groups to session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,13 @@ export default async function Home() {
   const session = await auth();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24 bg-library-splash bg-no-repeat bg-center">
+      <pre className="whitespace-pre-line break-all bg-white bg-opacity-80 p-4 rounded-lg">
+        {JSON.stringify(session, null, 2)}
+      </pre>
       <Card className="shadow-lg">
         <CardHeader>
           <CardTitle>
-            Welcome to Local Library {session?.user && session.user.name}
+            Welcome to Local Library{session?.user && `, ${session.user.name}`}
           </CardTitle>
         </CardHeader>
         <CardContent className="text-center">
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,39 @@
-import NextAuth from 'next-auth';
+/* eslint-disable no-unused-vars */
+import NextAuth, { DefaultSession } from 'next-auth';
 import Authentik from 'next-auth/providers/authentik';
 
+declare module 'next-auth' {
+  interface Session {
+    accessToken: string;
+    user: {
+      groups: string[];
+    } & DefaultSession['user'];
+  }
+
+  interface User {
+    groups: string[]
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Authentik],
+  providers: [Authentik({
+    profile(profile) {
+      return {
+        ...profile,
+        groups: profile.groups ?? ['user'],
+      }
+    }
+  })],
+  callbacks: {
+    jwt({ token, user, account }) {
+      let returnToken = token;
+      if (user && account) {
+        returnToken = { ...token, groups: user.groups, accessToken: account.access_token };
+      }
+      return returnToken;
+    },
+    session({ session, token }) {
+      return { ...session, accessToken: token.accessToken, user: { ...session.user, groups: token.groups as string[] }};
+    }
+  }
 });
